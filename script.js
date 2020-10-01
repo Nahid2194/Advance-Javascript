9771a436c9fb2ed23e2b7045f74622cf3227b347
Nahid2194 <nahidjc80@gmail.com>
2020-10-01 20:22:29 +0600

    true and false check

---
+++ script.js
@@ -0,0 +1,2 @@
+console.log("My Name is Nahid");
+console.log(21);
---
+++ true-false.js
@@ -0,0 +1,16 @@
+//fALSY
+//""
+// 0
+// null
+// undefined
+// false
+//truthy
+// true
+//"0"
+const value = "false";
+
+if (value) {
+    console.log("Condition is True");
+}else{
+    console.log("Condition is false");
+}
